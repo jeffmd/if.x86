acfdd6e1336c4ceabcb0bd8200dd700749459c00
Jeff Doyle <shelleydoyle@eastlink.ca>
2017-10-26 00:37:53 -0300

    literals can be inlinned during compile - added boot.fs which will boot if into forth mode - added words: lit ;opt - ;opt does not optimize tail call yet

---
+++ words/boot.fs
@@ -0,0 +1,78 @@
+dp 
+pname header dup $FF00 or $dp!
+  current @ @ dp!
+  cp dp!
+  smudge !
+  1 state h!
+      dp >r >r dup $FF00 or $dp! r> @ dp! r>  
+  [
+  ret,
+  uwid
+
+pname (create) current @ header
+  cp dp!
+  smudge !
+  1 state h!
+    pname current @ header cp dp!
+  [
+  ret,
+  uwid
+
+(create) ] 
+  smudge !
+  1 state h!
+    1 state h!
+  [
+  ret,
+  uwid
+
+(create) :
+  smudge !
+  ]
+    (create) smudge ! ]
+  [
+  ret,
+  uwid
+
+: cur@
+    current @
+  [
+  ;opt uwid
+
+: widf
+    cur@
+    @
+    dup
+    h@
+    rot and
+    swap
+    h!
+  [
+  ;opt uwid
+
+: immediate
+    $7FFF widf
+  [
+  ;opt uwid immediate
+
+: \
+    stib
+    nip
+    >in
+    h!
+  [
+  ;opt uwid immediate
+
+\ boot.fs - bootstrap the forth compiler
+\ header, (create), ] are created manually
+\ use (create) to make : then define the rest manually
+\ : can now be used to define a new word but must manually
+\ terminate the definition of a new word
+
+\ define ; which is used when finishing the compiling of a word
+: ;
+  \ change to interpret mode and override to compile [
+  [ pname [ findw nfa>xtf cxt ]
+  \ back in compile mode
+    ;opt uwid
+[ ;opt uwid immediate
