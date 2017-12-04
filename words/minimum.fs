a0a2eb5f7f3ef50f7e844a418e0dd39a8cba7ed8
Jeff Doyle <shelleydoyle@eastlink.ca>
2017-12-04 00:04:34 -0400

    minimum.fs now works with latest if

--- words/minimum.fs
+++ words/minimum.fs
@@ -1,10 +1,14 @@
 \ minimum.fs Forth words that make up minimum forth vocabulary
 
 
-( n min max -- f)
+( n min max -- f )
 \ check if n is within min..max
 : within
-    over - >a - a u<
+    over       \ n min max min 
+    - !x       \ n min diff  X: diff
+    pop -      \ n-min 
+    push x     \ n-min x
+    u<         \ flag
 ;
 
 \ emits a space (bl)
@@ -16,13 +20,14 @@
 \ only accepts positive values
 : spaces ( n -- )
     \ make sure a positive number
-    dup 0> and
+    push 0> and
+    push
     begin
     ?while
       space
-      1- 
+      d0 1- !d0 
     repeat
-    drop
+    nip
 ;
 
 \ pointer to current write position
@@ -32,26 +37,26 @@ var hld
 
 \ prepend character to pictured numeric output buffer
 : hold ( c -- )
-    -1 hld +!   
-    hld @ c!
+    push hld 1-!   
+    @ c!
 ;
 
 \ Address of the temporary scratch buffer.
 : pad ( -- a-addr )
-    here 20 +
+    here push 20 +
 ;
 
 \ initialize the pictured numeric output conversion process
-: <# ( -- )
-    pad hld !
+: <# ( n -- n )
+    push pad push hld ! pop
 ;
 
 \ pictured numeric output: convert one digit
 : # ( u1 -- u2 )
-    base@      ( u1 base )
-    u/mod      ( rem u2 )
-    swap       ( u2 rem )
-    #h hold    ( u2 )
+    push base@  ( u1 base )
+    u/mod       ( rem u2 )
+    swap        ( u2 rem )
+    #h hold pop ( u2 )
 ;
 
 \ pictured numeric output: convert all digits until 0 (zero) is reached
@@ -65,58 +70,57 @@ var hld
 
 \ Pictured Numeric Output: convert PNO buffer into an string
 : #> ( u1 -- addr count )
-    drop hld @ pad over -
+    hld @ push pad over -
 ;
 
 \ place a - in HLD if n is negative
 : sign ( n -- )
-    0< if [char] - hold then
+    0< ?if [char] - hold then
 ;
 
 \ singed PNO with cell numbers, right aligned in width w
 : .r ( wantsign n w -- )
-    >r   ( wantsign n ) ( R: w )
-    <#
-    #s   ( wantsign 0 )
-    swap ( 0 wantsign )
-    sign ( 0 )
-    #>   ( addr len )
-    r>   ( addr len w )  ( R: )
-    over ( addr len w len )
-    -    ( addr len spaces )
-    spaces ( addr len )
-    type  ( )
+    rpush pop  ( wantsign n ) ( R: w )
+    <# #s      ( wantsign 0 )
+    pop sign   ( ? )
+    #>         ( addr len )
+    push rpop  ( addr len w )  ( R: )
+    over       ( addr len w len )
+    -          ( addr len spaces )
+    spaces     ( addr len ? )
+    pop type   ( )
     space
 ;
 
 \ unsigned PNO with single cell numbers
 : u. ( u -- )
-    0      ( n 0 ) \ want unsigned
-    tuck   ( 0 n 0 )
+    push2 0 ( n n 0 ) \ want unsigned
+    !d1     ( 0 n 0 )
     .r 
 ;
 
 
 \ singed PNO with single cell numbers
 : .  ( n -- )
-    dup      ( n n )
-    abs      ( n n' )
-    0        ( n n' 0 ) \ not right aligned
+    push      ( n n )
+    abs       ( n n' )
+    push 0    ( n n' 0 ) \ not right aligned
     .r
 ;
 
 : .s  ( -- )
-    sp@         ( limit ) \ setup limit
+    push        ( ?  ? )
+    sp          ( ? limit ) \ setup limit
     dcell-
-    sp0         ( limit counter )
+    push sp0    ( ? limit counter )
     begin
-      dcell-    ( limit counter-4 )
-      2over     ( limit counter-4 limit counter-4 )
-      <>        ( limit counter-4 flag )
-      while
-        dup     ( limit counter-4 counter-4 )
-        @       ( limit counter-4 val )
-        u.      ( limit counter-4 )
+      dcell-    ( ? limit counter-4 )
+      2over     ( ? limit counter-4 limit counter-4 )
+      <>        ( ? limit counter-4 flag )
+      ?while
+        d0      ( ? limit counter-4 counter-4 )
+        @       ( ? limit counter-4 val )
+        u. pop  ( ? limit counter-4 )
     repeat
-    2drop
+    nip2 pop
 ;
