0a21bfc5a91ab04129ea9b2842b2d5a86458ad0b
Jeff Doyle <shelleydoyle@eastlink.ca>
2017-12-07 11:18:25 -0400

    enable loading of debugtools.fs when starting if

--- words/start.fs
+++ words/start.fs
@@ -3,6 +3,6 @@ include words/core.fs
 include words/compiler.fs 
 include words/minimum.fs 
 include words/vocabulary.fs
-\ include words/debugtools.fs 
+include words/debugtools.fs 
 \ include words/tasker.fs
 \ include words/gpio.fs
