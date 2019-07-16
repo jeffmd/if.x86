703e27fc31fde716a07b03ca40a57835e74c6c05
Jeff Doyle <shelleydoyle@eastlink.ca>
2019-07-16 11:41:43 -0300

    minimum.fs:  now using register ops

--- words/minimum.fs
+++ words/minimum.fs
@@ -3,11 +3,12 @@
 
 ( n min max -- f )
 \ check if n is within min..max
+\ flag is 1 if min <= n <= max
 : within
-    pop.y      \ n min max Y: min
-    -y !x      \ n min diff X: diff
-    d0 -y !d0  \ n-min min n-min 
-    x          \ n-min diff
+    popy       \ n max Y: min
+    w-=y       \ n diff 
+    x=d0 x-=y 
+    d0=x       \ n-min diff 
     u<         \ flag
 ;
 
@@ -20,12 +21,12 @@
 \ only accepts positive values
 : spaces ( n -- )
     \ make sure a positive number
-    !y 0> and.y   ( n' Y:n )
+    y=w 0> w&=y   ( n' Y:n )
     push          ( n' n' )
     begin
     ?while
       space
-      d0 1- !d0 
+      d0 1- d0=w 
     repeat
     nip
 ;
@@ -37,25 +38,26 @@ var hld
 
 \ prepend character to pictured numeric output buffer
 : hold ( c -- )
-    !y hld 1-!   
-    @ y.c!
+    y=w hld
+    x=@w x-=1 @w=x   
+    y c@x=w
 ;
 
 \ Address of the temporary scratch buffer.
 : pad ( -- a-addr )
-    y= 20 here  +y
+    y= 20 here  w+=y
 ;
 
 \ initialize the pictured numeric output conversion process
 : <# ( n -- n )
-    push pad !y hld y.! pop
+    push pad y=w hld @w=y pop
 ;
 
 \ pictured numeric output: convert one digit
 : # ( u1 -- u2 )
     push base@  ( u1 base )
     u/mod       ( rem u2 )
-    swap        ( u2 rem )
+    x=w d0 d0=x ( u2 rem )
     #h hold pop ( u2 )
 ;
 
@@ -70,8 +72,8 @@ var hld
 
 \ Pictured Numeric Output: convert PNO buffer into an string
 : #> ( u1 -- addr count )
-    hld @ !x    \ addr Y: addr  
-    push pad -x \ addr pad-addr
+    hld @ x=w     \ addr Y: addr  
+    push pad w-=x \ addr pad-addr
 ;
 
 \ place a - in HLD if n is negative
@@ -79,15 +81,15 @@ var hld
     0< ?if [char] - hold then
 ;
 
-\ singed PNO with cell numbers, right aligned in width w
+\ signed PNO with cell numbers, right aligned in width w
 : .r ( wantsign n w -- )
     rpush pop  ( wantsign n ) ( R: w )
     <# #s      ( wantsign 0 )
     pop sign   ( ? )
     #>         ( addr len )
     push rpop  ( addr len w )  ( R: )
-    d0!y       ( addr len w Y:len )
-    -y         ( addr len spaces )
+    y=d0       ( addr len w Y:len )
+    w-=y       ( addr len spaces )
     spaces     ( addr len ? )
     pop type   ( )
     space
@@ -95,8 +97,8 @@ var hld
 
 \ unsigned PNO with single cell numbers
 : u. ( u -- )
-    push2 0 ( u u 0 ) \ want unsigned
-    !d1     ( 0 u 0 )
+    push push 0 ( u u 0 ) \ want unsigned
+    d1=w        ( 0 u 0 )
     .r 
 ;
 
@@ -111,13 +113,14 @@ var hld
 
 : .s  ( -- )
     push        ( ?  ? )
-    sp          ( ? limit ) \ setup limit
+    dsp         ( ? limit ) \ setup limit
     dcell-
-    push sp0    ( ? limit counter )
+    push dsp0   ( ? limit counter )
     begin
       dcell-    ( ? limit counter-4 )
-      2over     ( ? limit counter-4 limit counter-4 )
-      <>        ( ? limit counter-4 flag )
+      y=d0 push
+      pushy     ( ? limit counter-4 limit counter-4 )
+      !=        ( ? limit counter-4 flag )
       ?while
         d0      ( ? limit counter-4 counter-4 )
         @       ( ? limit counter-4 val )
